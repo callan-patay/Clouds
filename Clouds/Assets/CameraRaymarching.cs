8d5a9d23286745390c65ddee198286062cc0042f
callan-patay <scalywags123@googlemail.com>
2018-11-19 17:13:17 +0000

    initial

---
+++ Clouds/Assets/CameraRaymarching.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Effects/Raymarch (Generic Complete)")]
+public class CameraRaymarching : MonoBehaviour
+{
+
+
+    public Transform SunLight;
+
+    [SerializeField]
+    private Shader _EffectShader;
+    [SerializeField]
+    private Texture2D _MaterialColorRamp;
+    [SerializeField]
+    private Texture2D _PerfColorRamp;
+    [SerializeField]
+    private float _RaymarchDrawDistance = 40;
+    [SerializeField]
+    private bool _DebugPerformance = false;
+    [SerializeField]
+    private Texture3D _texture;
+
+
+
+    public int size = 256;
+    public float scale = 20f;
+    // http://flafla2.github.io/2016/10/01/raymarching.html#introduction-to-raymarching
+
+    void Start()
+    {
+        _texture = generateClouds(size);
+
+
+
+    }
+
+
+    public Material EffectMaterial
+    {
+        get
+        {
+            if (!_EffectMaterial && _EffectShader)
+            {
+                _EffectMaterial = new Material(_EffectShader);
+                _EffectMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            return _EffectMaterial;
+        }
+    }
+    private Material _EffectMaterial;
+    public Camera CurrentCamera
+    {
+        get
+        {
+            if (!_CurrentCamera)
+                _CurrentCamera = GetComponent<Camera>();
+            return _CurrentCamera;
+        }
+    }
+    private Camera _CurrentCamera;
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Matrix4x4 corners = GetFrustumCorners(CurrentCamera);
+        Vector3 pos = CurrentCamera.transform.position;
+
+        for (int x = 0; x < 4; x++)
+        {
+            corners.SetRow(x, _CurrentCamera.cameraToWorldMatrix * corners.GetRow(x));
+            Gizmos.DrawLine(pos, pos + (Vector3)(corners.GetRow(x)));
+        }
+
+        //UNCOMMENT TO DEBUG RAY DIRECTIONS
+        Gizmos.color = Color.red;
+        int n = 10; // # of intervals
+        for (int x = 1; x < n; x++) {
+            float i_x = (float)x / (float)n;
+
+            var w_top = Vector3.Lerp(corners.GetRow(0), corners.GetRow(1), i_x);
+            var w_bot = Vector3.Lerp(corners.GetRow(3), corners.GetRow(2), i_x);
+            for (int y = 1; y < n; y++) {
+                float i_y = (float)y / (float)n;
+                
+                var w = Vector3.Lerp(w_top, w_bot, i_y).normalized;
+                Gizmos.DrawLine(pos + (Vector3)w, pos + (Vector3)w * 1.2f);
+            }
+        }
+       
+    }
+
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!EffectMaterial)
+        {
+            Graphics.Blit(source, destination); // do nothing
+            return;
+        }
+
+        EffectMaterial.SetMatrix("_FrustumCornersES", GetFrustumCorners(CurrentCamera));
+        EffectMaterial.SetMatrix("_CameraInvViewMatrix", CurrentCamera.cameraToWorldMatrix);
+        EffectMaterial.SetVector("_CameraWS", CurrentCamera.transform.position);
+        EffectMaterial.SetVector("_LightDir", SunLight ? SunLight.forward : Vector3.down);
+        EffectMaterial.SetTexture("_Volume", _texture);
+        CustomGraphicsBlit(source, destination, EffectMaterial, 0); // use given effect shader as image effect
+    }
+
+
+    static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial, int passNr)
+    {
+        RenderTexture.active = dest;
+
+        fxMaterial.SetTexture("_MainTex", source);
+        GL.PushMatrix();
+        GL.LoadOrtho(); // Note: z value of vertices don't make a difference because we are using ortho projection
+
+        fxMaterial.SetPass(passNr);
+
+        GL.Begin(GL.QUADS);
+
+        // Here, GL.MultitexCoord2(0, x, y) assigns the value (x, y) to the TEXCOORD0 slot in the shader.
+        // GL.Vertex3(x,y,z) queues up a vertex at position (x, y, z) to be drawn.  Note that we are storing
+        // our own custom frustum information in the z coordinate.
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    private Matrix4x4 GetFrustumCorners(Camera cam)
+    {
+        float camFov = cam.fieldOfView;
+        float camAspect = cam.aspect;
+
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fovWHalf = camFov * 0.5f;
+
+        float tan_fov = Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+        Vector3 toRight = Vector3.right * tan_fov * camAspect;
+        Vector3 toTop = Vector3.up * tan_fov;
+
+        Vector3 topLeft = (-Vector3.forward - toRight + toTop);
+        Vector3 topRight = (-Vector3.forward + toRight + toTop);
+        Vector3 bottomRight = (-Vector3.forward + toRight - toTop);
+        Vector3 bottomLeft = (-Vector3.forward - toRight - toTop);
+
+        frustumCorners.SetRow(0, topLeft);
+        frustumCorners.SetRow(1, topRight);
+        frustumCorners.SetRow(2, bottomRight);
+        frustumCorners.SetRow(3, bottomLeft);
+
+        return frustumCorners;
+    }
+
+    Texture3D generateClouds(int size)
+    {
+        Color[] colorArray = new Color[size * size * size];
+        _texture = new Texture3D(size, size, size, TextureFormat.RGBA32, true);
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                for (int z = 0; z < size; z++)
+                {
+                    float p = Perlin3D((float)x / size, (float)y / size, (float)z / size, scale);
+                    Color c = new Color(p, p, p, p);
+                    colorArray[x + (y * size) + (z * size * size)] = c;
+                }
+            }
+        }
+
+        _texture.SetPixels(colorArray);
+        _texture.Apply();
+        return _texture;
+
+    }
+
+
+    public static float Perlin3D(float x, float y, float z, float scale)
+    {
+        float AB = Mathf.PerlinNoise(x * scale, y * scale);
+        float BC = Mathf.PerlinNoise(y * scale, z * scale);
+        float AC = Mathf.PerlinNoise(x * scale, z * scale);
+
+        float BA = Mathf.PerlinNoise(y * scale, x * scale);
+        float CB = Mathf.PerlinNoise(z * scale, y * scale);
+        float CA = Mathf.PerlinNoise(z * scale, x * scale);
+
+        float ABC = AB + BC + AC + BA + CB + CA;
+        return ABC / 6f;
+
+    }
+}
